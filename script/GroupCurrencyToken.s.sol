ad86a40e483956a3f10d801968fee4a1dd19457d
alculexum <alexander.culum@web.de>
2022-11-26 23:10:46 +0100

    Refactoring.

--- script/GroupCurrencyToken.s.sol
+++ script/GroupCurrencyToken.s.sol
@@ -14,8 +14,8 @@ contract GroupCurrencyTokenDeploy is Script {
     function run() external {
         // Setup & Deploy
         vm.startPrank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
-        GroupCurrencyTokenFactory gctf = new GroupCurrencyTokenFactory();
         vm.recordLogs();
+        GroupCurrencyTokenFactory gctf = new GroupCurrencyTokenFactory();
         IHub hub = IHub(0x29b9a7fBb8995b2423a71cC17cf9810798F6C543);
         gctf.createGroupCurrencyToken(address(hub), 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 0, "GCT", "GCT");
         
@@ -28,12 +28,10 @@ contract GroupCurrencyTokenDeploy is Script {
         cols[0] = token;
         uint256[] memory tokens = new uint256[](1);
         tokens[0] = 50000000000000000000;
-        
-        ERC20 tokenCtr = ERC20(token);
         vm.stopPrank();
         
         vm.startPrank(0x249fA3ecD95a53F742707D53688FCafbBd072f33);
-        tokenCtr.transfer(address(gct), 50000000000000000000);
+        ERC20(token).transfer(address(gct), 50000000000000000000);
         gct.mint(cols, tokens);
         
         vm.stopPrank();
